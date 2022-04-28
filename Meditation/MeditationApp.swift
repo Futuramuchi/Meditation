5dca6f4d1fdaf3ba3536c8bf3ba93f5783afc8ad
Kikimora32 <82947993+Kikimora32@users.noreply.github.com>
2022-04-28 09:14:16 +0300

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Student on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
