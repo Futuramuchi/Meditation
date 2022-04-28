7c09829a3a95c6370269f48fd5f72313f786b0f7
Kikimora32 <82947993+Kikimora32@users.noreply.github.com>
2022-04-28 12:23:50 +0300

    Uploading

---
+++ Meditation/UserProfile/MusicView.swift
@@ -0,0 +1,20 @@
+//
+//  MusicView.swift
+//  Meditation
+//
+//  Created by Student on 28.04.2022.
+//
+
+import SwiftUI
+
+struct MusicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MusicView_Previews: PreviewProvider {
+    static var previews: some View {
+        MusicView()
+    }
+}
---
+++ Meditation/UserProfile/UserNewsView.swift
@@ -0,0 +1,20 @@
+//
+//  UserNewsView.swift
+//  Meditation
+//
+//  Created by Student on 28.04.2022.
+//
+
+import SwiftUI
+
+struct UserNewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserNewsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserNewsView()
+    }
+}
